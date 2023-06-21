a14150bdf264dec5d25b36edfa4053942591e4f5
Halim258 <mohamed.halme204204@gmail.com>
2023-06-21 22:06:36 +0200

    fix * icon

---
+++ script.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector('.hamburger');
+const navigation = document.querySelector('.navigation');
+
+hamburger.addEventListener('click', () => {
+  hamburger.classList.toggle('active');
+  navigation.classList.toggle('active');
+})
+
+document.querySelectorAll('.navigation-link').forEach(n=> n. 
+  addEventListener('click',() => {
+    hamburger.classList.remove('active');
+    navigation.classList.remove('active');
+  }))
